df4a04942dabdf6f2c91de7fd87d8af2ef5526b6
Fabricio Colombo <fabriciocolombo0096@gmail.com>
2024-03-21 14:31:48 -0300

    save

---
+++ database/cliente.py
@@ -0,0 +1,12 @@
+CLIENTES = [
+    {"ID": 1, "Nome": "João Silva", "Email": "joao.silva@example.com"},
+    {"ID": 2, "Nome": "Maria Souza", "Email": "maria.souza@example.com"},
+    {"ID": 3, "Nome": "Pedro Santos", "Email": "pedro.santos@example.com"},
+    {"ID": 4, "Nome": "Ana Oliveira", "Email": "ana.oliveira@example.com"},
+    {"ID": 5, "Nome": "Carlos Rodrigues", "Email": "carlos.rodrigues@example.com"},
+    {"ID": 6, "Nome": "Beatriz Almeida", "Email": "beatriz.almeida@example.com"},
+    {"ID": 7, "Nome": "Paulo Martins", "Email": "paulo.martins@example.com"},
+    {"ID": 8, "Nome": "Luana Carvalho", "Email": "luana.carvalho@example.com"},
+    {"ID": 9, "Nome": "Daniel Ribeiro", "Email": "daniel.ribeiro@example.com"},
+    {"ID": 10, "Nome": "Fernanda Silva", "Email": "fernanda.silva@example.com"}
+]
--- routes/cliente.py
+++ routes/cliente.py
@@ -1,4 +1,6 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+from database.cliente import CLIENTES
+
 
 cliente_route = Blueprint('cliente', __name__)
 
@@ -16,7 +18,7 @@ Rota de Clientes
 
 @cliente_route.route('/')
 def lista_clientes():
-    pass
+    return render_template('lista_clientes.html', clientes=CLIENTES)
 
 @cliente_route.route('/', methods= ['POST'])
 def inserir_cliente():
@@ -24,15 +26,16 @@ def inserir_cliente():
 
 @cliente_route.route('/new')
 def form_cliente():
-    pass
+    return render_template('form_cliente.html')
 
 @cliente_route.route('/<int:cliente_id>')
 def detalhe_cliente(cliente_id):
-    pass
+    return render_template('detalhe_cliente.html', cliente_id=cliente_id)
+
 
 @cliente_route.route('/<int:cliente_id>/edit')
 def form_edit_cliente(cliente_id):
-    pass
+    return render_template('form_edit_cliente.html', cliente_id=cliente_id)
 
 @cliente_route.route('/<int:cliente_id>/update', methods=['PUT'])
 def atualizar_cliente(cliente_id):
