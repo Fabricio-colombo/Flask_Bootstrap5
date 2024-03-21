9200223739d06bf484b8c7d2dfc95fb4e66e6b60
Fabricio Colombo <fabriciocolombo0096@gmail.com>
2024-03-21 17:05:37 -0300

    sucesso

--- database/cliente.py
+++ database/cliente.py
@@ -1,12 +1,12 @@
 CLIENTES = [
-    {"ID": 1, "nome": "João Silva", "email": "joao.silva@example.com"},
-    {"ID": 2, "nome": "Maria Souza", "email": "maria.souza@example.com"},
-    {"ID": 3, "nome": "Pedro Santos", "email": "pedro.santos@example.com"},
-    {"ID": 4, "nome": "Ana Oliveira", "email": "ana.oliveira@example.com"},
-    {"ID": 5, "nome": "Carlos Rodrigues", "email": "carlos.rodrigues@example.com"},
-    {"ID": 6, "nome": "Beatriz Almeida", "email": "beatriz.almeida@example.com"},
-    {"ID": 7, "nome": "Paulo Martins", "email": "paulo.martins@example.com"},
-    {"ID": 8, "nome": "Luana Carvalho", "email": "luana.carvalho@example.com"},
-    {"ID": 9, "nome": "Daniel Ribeiro", "email": "daniel.ribeiro@example.com"},
-    {"ID": 10, "nome": "Fernanda Silva", "email": "fernanda.silva@example.com"}
+    {"id": 1, "nome": "João Silva", "email": "joao.silva@example.com"},
+    {"id": 2, "nome": "Maria Souza", "email": "maria.souza@example.com"},
+    {"id": 3, "nome": "Pedro Santos", "email": "pedro.santos@example.com"},
+    {"id": 4, "nome": "Ana Oliveira", "email": "ana.oliveira@example.com"},
+    {"id": 5, "nome": "Carlos Rodrigues", "email": "carlos.rodrigues@example.com"},
+    {"id": 6, "nome": "Beatriz Almeida", "email": "beatriz.almeida@example.com"},
+    {"id": 7, "nome": "Paulo Martins", "email": "paulo.martins@example.com"},
+    {"id": 8, "nome": "Luana Carvalho", "email": "luana.carvalho@example.com"},
+    {"id": 9, "nome": "Daniel Ribeiro", "email": "daniel.ribeiro@example.com"},
+    {"id": 10, "nome": "Fernanda Silva", "email": "fernanda.silva@example.com"}
 ]
