dcefef78b622334c622d427bc1d2c5a722036396
Fabricio Colombo <fabriciocolombo0096@gmail.com>
2024-03-21 17:12:36 -0300

    x

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ app = Flask(__name__)
 app.register_blueprint(home_route)
 app.register_blueprint(cliente_route, url_prefix="/clientes")
 
-
-app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
