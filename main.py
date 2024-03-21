d32aa8ffa156a3847826d62c16f53f0be626464d
Fabricio Colombo <fabriciocolombo0096@gmail.com>
2024-03-21 17:18:24 -0300

    l

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template
-from routes.home import home_route
-from routes.cliente import cliente_route
+def create_app():
+    from flask import Flask, render_template
+    from routes.home import home_route
+    from routes.cliente import cliente_route
+    
+    app = Flask(__name__)
 
-app = Flask(__name__)
+    app.register_blueprint(home_route)
+    app.register_blueprint(cliente_route, url_prefix="/clientes")
 
-app.register_blueprint(home_route)
-app.register_blueprint(cliente_route, url_prefix="/clientes")
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    return app
