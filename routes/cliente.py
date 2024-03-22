bf4c8548d61a39e1efe9e112b80b7d507b855fd1
Fabricio Colombo <fabriciocolombo0096@gmail.com>
2024-03-22 09:10:26 -0300

    update

--- routes/cliente.py
+++ routes/cliente.py
@@ -55,7 +55,16 @@ def form_edit_cliente(cliente_id):
 
 @cliente_route.route('/<int:cliente_id>/update', methods=['PUT'])
 def atualizar_cliente(cliente_id):
-    pass
+    cliente = None
+    data = request.json
+    for c in CLIENTES:
+        if c['id'] == cliente_id:
+            c['nome'] = data['nome']
+            c['email'] = data['email']
+            cliente = c    
+    
+    return render_template('item_cliente.html', cliente=cliente)
+
 
 @cliente_route.route('/<int:cliente_id>/delete', methods=['DELETE'])
 def deletar_cliente(cliente_id):
