7e5d2ce03da0187f75d8208a889f291a970c2a6a
Fabricio Colombo <fabriciocolombo0096@gmail.com>
2024-03-22 10:16:38 -0300

    modal

--- routes/cliente.py
+++ routes/cliente.py
@@ -41,7 +41,9 @@ def form_cliente():
 
 @cliente_route.route('/<int:cliente_id>')
 def detalhe_cliente(cliente_id):
-    return render_template('detalhe_cliente.html', cliente_id=cliente_id)
+    
+    cliente = list(filter (lambda c: c['id'] == cliente_id, CLIENTES))[0]
+    return render_template('detalhe_cliente.html', cliente=cliente)
 
 
 @cliente_route.route('/<int:cliente_id>/edit')
